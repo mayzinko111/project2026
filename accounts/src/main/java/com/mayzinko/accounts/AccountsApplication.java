88be383663c793115e0074dcebfccfd6ff03e18a
mayzinko111 <49020629+mayzinko111@users.noreply.github.com>
2025-11-03 13:07:59 +0800

    [Commit-5] add Account service apis
    - use spring cloud config server
    - use jpa and h2 databbase
    - register eureka server
    - use dto, mapper, exception handling, input validation

--- accounts/src/main/java/com/mayzinko/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/mayzinko/accounts/AccountsApplication.java
@@ -1,8 +1,14 @@
 package com.mayzinko.accounts;
 
+import com.mayzinko.accounts.dto.AccountsContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
 @SpringBootApplication
 public class AccountsApplication {
 
